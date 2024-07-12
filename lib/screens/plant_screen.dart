e2cd1d41190bcb98576f8cae096a66504779093a
shiroyuki24 <davemaniego.0000@gmail.com>
2024-07-12 16:37:22 +0800

    merged uses and benefits content state and added process content state

--- lib/screens/plant_screen.dart
+++ lib/screens/plant_screen.dart
@@ -10,10 +10,15 @@ class PlantScreen extends StatefulWidget {
   _PlantScreenState createState() => _PlantScreenState();
 }
 
-enum ContentState { description, uses, benefits }
+enum ContentState {
+  description,
+  usesAndBenefits,
+  process,
+}
 
 class _PlantScreenState extends State<PlantScreen> {
   ContentState _contentState = ContentState.description;
+  ContentState _previousState = ContentState.description;
   final ScrollController _scrollController = ScrollController();
 
   void _scrollToTop() {
@@ -23,6 +28,40 @@ class _PlantScreenState extends State<PlantScreen> {
         duration: Duration(milliseconds: 300),
         curve: Curves.easeOut,
       );
+    });
+  }
+
+  void _nextState() {
+    setState(() {
+      if (_contentState == ContentState.description) {
+        _previousState = _contentState;
+        _contentState = ContentState.usesAndBenefits;
+      } else if (_contentState == ContentState.usesAndBenefits) {
+        _previousState = _contentState;
+        _contentState = ContentState.process;
+      }
+      _scrollToTop();
+    });
+  }
+
+  void _processState() {
+    setState(() {
+      _previousState = _contentState;
+      _contentState = ContentState.process;
+      _scrollToTop();
+    });
+  }
+
+  void _previousStateFunction() {
+    setState(() {
+      if (_contentState == ContentState.process) {
+        _contentState = _previousState;
+      } else if (_contentState == ContentState.usesAndBenefits) {
+        _contentState = ContentState.description;
+      } else {
+        Navigator.pop(context);
+      }
+      _scrollToTop();
     });
   }
 
@@ -79,25 +118,14 @@ class _PlantScreenState extends State<PlantScreen> {
                       children: [
                         IconButton(
                           icon: Icon(Icons.arrow_back, color: Colors.black),
-                          onPressed: () {
-                            setState(() {
-                              if (_contentState == ContentState.benefits) {
-                                _contentState = ContentState.uses;
-                              } else if (_contentState == ContentState.uses) {
-                                _contentState = ContentState.description;
-                              } else {
-                                Navigator.pop(context);
-                              }
-                              _scrollToTop();
-                            });
-                          },
+                          onPressed: _previousStateFunction,
                         ),
                         SizedBox(width: 10),
                         Text(
                           widget.plant.eng_name,
                           style: TextStyle(
                             color: Colors.black,
-                            fontSize: 24,
+                            fontSize: 28,
                             fontWeight: FontWeight.bold,
                             fontFamily: 'Montserrat',
                           ),
@@ -114,12 +142,13 @@ class _PlantScreenState extends State<PlantScreen> {
                             Text(
                               _contentState == ContentState.description
                                   ? 'Description'
-                                  : _contentState == ContentState.uses
-                                      ? 'Uses'
-                                      : 'Benefits',
+                                  : _contentState ==
+                                          ContentState.usesAndBenefits
+                                      ? 'Uses & Benefits'
+                                      : 'Process',
                               style: TextStyle(
                                 color: Colors.black,
-                                fontSize: 28,
+                                fontSize: 25,
                                 fontWeight: FontWeight.bold,
                                 fontFamily: 'Montserrat',
                               ),
@@ -128,7 +157,7 @@ class _PlantScreenState extends State<PlantScreen> {
                               Column(
                                 crossAxisAlignment: CrossAxisAlignment.start,
                                 children: [
-                                  SizedBox(height: 10),
+                                  SizedBox(height: 15),
                                   Text(
                                     'Tagalog name: ${widget.plant.tag_name}',
                                     style: TextStyle(
@@ -148,110 +177,126 @@ class _PlantScreenState extends State<PlantScreen> {
                                       color: Colors.black87,
                                     ),
                                   ),
+                                  SizedBox(height: 25),
+                                  Text(
+                                    widget.plant.description ?? '',
+                                    style: TextStyle(
+                                      fontSize: 18,
+                                      fontFamily: 'Montserrat',
+                                      color: Colors.black87,
+                                    ),
+                                    textAlign: TextAlign.justify,
+                                  ),
                                 ],
                               ),
-                            SizedBox(height: 35),
-                            Text(
-                              _contentState == ContentState.description
-                                  ? widget.plant.description ?? ''
-                                  : _contentState == ContentState.uses
-                                      ? widget.plant.uses ?? ''
-                                      : widget.plant.benefits ?? '',
-                              style: TextStyle(
-                                fontSize: 18,
-                                fontFamily: 'Montserrat',
-                                color: Colors.black87,
-                              ),
-                              textAlign: TextAlign.justify,
-                            ),
-                            SizedBox(height: 20),
-                            Row(
-                              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                              children: [
-                                if (_contentState == ContentState.description)
-                                  ElevatedButton(
-                                    onPressed: () {
-                                      setState(() {
-                                        _contentState = ContentState.uses;
-                                      });
-                                      _scrollToTop();
-                                    },
-                                    style: ElevatedButton.styleFrom(
-                                      backgroundColor: Colors.green,
-                                      foregroundColor: Colors.white,
-                                      shape: RoundedRectangleBorder(
-                                        borderRadius: BorderRadius.circular(10),
-                                      ),
-                                      padding: EdgeInsets.symmetric(
-                                          horizontal: 32, vertical: 16),
-                                      shadowColor:
-                                          Colors.black.withOpacity(0.3),
-                                      elevation: 5,
-                                    ),
-                                    child: Text(
-                                      'Uses & Benefits',
-                                      style: TextStyle(
-                                        fontFamily: 'Karla',
-                                        fontSize: 20,
-                                        fontWeight: FontWeight.w500,
-                                      ),
-                                    ),
-                                  ),
-                                if (_contentState == ContentState.uses)
-                                  ElevatedButton(
-                                    onPressed: () {
-                                      setState(() {
-                                        _contentState = ContentState.benefits;
-                                      });
-                                      _scrollToTop();
-                                    },
-                                    style: ElevatedButton.styleFrom(
-                                      backgroundColor: Colors.green,
-                                      foregroundColor: Colors.white,
-                                      shape: RoundedRectangleBorder(
-                                        borderRadius: BorderRadius.circular(10),
-                                      ),
-                                      padding: EdgeInsets.symmetric(
-                                          horizontal: 32, vertical: 16),
-                                      shadowColor:
-                                          Colors.black.withOpacity(0.3),
-                                      elevation: 5,
-                                    ),
-                                    child: Text(
-                                      'Benefits',
-                                      style: TextStyle(
-                                        fontFamily: 'Karla',
-                                        fontSize: 20,
-                                        fontWeight: FontWeight.w500,
-                                      ),
-                                    ),
-                                  ),
-                                ElevatedButton(
-                                  onPressed: () {
-                                    // Implement Process functionality
-                                  },
-                                  style: ElevatedButton.styleFrom(
-                                    backgroundColor: Colors.green,
-                                    foregroundColor: Colors.white,
-                                    shape: RoundedRectangleBorder(
-                                      borderRadius: BorderRadius.circular(10),
-                                    ),
-                                    padding: EdgeInsets.symmetric(
-                                        horizontal: 32, vertical: 16),
-                                    shadowColor: Colors.black.withOpacity(0.3),
-                                    elevation: 5,
-                                  ),
-                                  child: Text(
-                                    'Process',
+                            if (_contentState == ContentState.usesAndBenefits)
+                              Column(
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
+                                  SizedBox(height: 10),
+                                  Text(
+                                    widget.plant.uses ?? '',
                                     style: TextStyle(
-                                      fontFamily: 'Karla',
-                                      fontSize: 20,
-                                      fontWeight: FontWeight.w500,
+                                      fontSize: 18,
+                                      fontFamily: 'Montserrat',
+                                      color: Colors.black87,
+                                    ),
+                                    textAlign: TextAlign.justify,
+                                  ),
+                                  SizedBox(height: 25),
+                                  Text(
+                                    'Benefits',
+                                    style: TextStyle(
+                                      color: Colors.black,
+                                      fontSize: 25,
+                                      fontWeight: FontWeight.bold,
+                                      fontFamily: 'Montserrat',
                                     ),
                                   ),
-                                ),
-                              ],
-                            ),
+                                  SizedBox(height: 20),
+                                  Text(
+                                    widget.plant.benefits ?? '',
+                                    style: TextStyle(
+                                      fontSize: 18,
+                                      fontFamily: 'Montserrat',
+                                      color: Colors.black87,
+                                    ),
+                                    textAlign: TextAlign.justify,
+                                  ),
+                                ],
+                              ),
+                            if (_contentState == ContentState.process)
+                              Column(
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
+                                  SizedBox(height: 20),
+                                  Text(
+                                    widget.plant.process ?? '',
+                                    style: TextStyle(
+                                      fontSize: 18,
+                                      fontFamily: 'Montserrat',
+                                      color: Colors.black87,
+                                    ),
+                                    textAlign: TextAlign.justify,
+                                  ),
+                                ],
+                              ),
+                            SizedBox(height: 20),
+                            if (_contentState != ContentState.process)
+                              Row(
+                                mainAxisAlignment:
+                                    MainAxisAlignment.spaceEvenly,
+                                children: [
+                                  if (_contentState == ContentState.description)
+                                    ElevatedButton(
+                                      onPressed: _nextState,
+                                      style: ElevatedButton.styleFrom(
+                                        backgroundColor: Colors.green,
+                                        foregroundColor: Colors.white,
+                                        shape: RoundedRectangleBorder(
+                                          borderRadius:
+                                              BorderRadius.circular(10),
+                                        ),
+                                        padding: EdgeInsets.symmetric(
+                                            horizontal: 32, vertical: 16),
+                                        shadowColor:
+                                            Colors.black.withOpacity(0.3),
+                                        elevation: 5,
+                                      ),
+                                      child: Text(
+                                        'Uses & Benefits',
+                                        style: TextStyle(
+                                          fontFamily: 'Karla',
+                                          fontSize: 20,
+                                          fontWeight: FontWeight.w500,
+                                        ),
+                                      ),
+                                    ),
+                                  ElevatedButton(
+                                    onPressed: _processState,
+                                    style: ElevatedButton.styleFrom(
+                                      backgroundColor: Colors.green,
+                                      foregroundColor: Colors.white,
+                                      shape: RoundedRectangleBorder(
+                                        borderRadius: BorderRadius.circular(10),
+                                      ),
+                                      padding: EdgeInsets.symmetric(
+                                          horizontal: 32, vertical: 16),
+                                      shadowColor:
+                                          Colors.black.withOpacity(0.3),
+                                      elevation: 5,
+                                    ),
+                                    child: Text(
+                                      'Process',
+                                      style: TextStyle(
+                                        fontFamily: 'Karla',
+                                        fontSize: 20,
+                                        fontWeight: FontWeight.w500,
+                                      ),
+                                    ),
+                                  ),
+                                ],
+                              ),
                             SizedBox(height: 20),
                             Container(
                               decoration: BoxDecoration(
